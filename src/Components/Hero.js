3881eb6ea2c0a5d5b67aa2869aecec49b2b7c0ff
Akshay anand <akshay1622004@gmail.com>
2024-10-17 20:40:09 +0530

    hero section

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import {Canvas, useFrame} from '@react-three/fiber'
+import {OrbitControls, PerspectiveCamera} from '@react-three/drei'
+import HackerRoom  from '../Models3D/HackerRoom.js'
+import CanvasLoader from '../Models3D/CanvasLoader.js'
 
 export const Hero = () => {
   return (
     <section className='min-h-screen w-full flex flex-col relative'>
         <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 sm:px-10 px-5 gap-3'>
             <p className='sm:text-3xl text-2xl font-medium text-white text-center font-generalsans'>Hi, I am Akshay <span className='waving-hand'>👋</span> </p>
-            <p className='hero_tag text-grey_gradient'> Building Products & Transforming Lives</p>
+            <p className='hero_tag text-gray_gradient'> Building Products & Transforming Lives</p>
         </div>
 
         <div className='w-full h-full absolute inset-0'>
-            
+            <Canvas className='w-full h-full'>
+
+                <Suspense fallback={<CanvasLoader/>}>
+                <PerspectiveCamera makeDefault position={[0,0,30]}/>
+
+                <HackerRoom/>
+                </Suspense>
+            </Canvas>   
         </div>
     </section>
   )
