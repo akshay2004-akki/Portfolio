866ec86d73e8b09771fe119c59945062e67f801f
Akshay anand <akshay1622004@gmail.com>
2024-10-17 20:44:46 +0530

    corrected

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -9,7 +9,7 @@ export const Hero = () => {
     <section className='min-h-screen w-full flex flex-col relative'>
         <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 sm:px-10 px-5 gap-3'>
             <p className='sm:text-3xl text-2xl font-medium text-white text-center font-generalsans'>Hi, I am Akshay <span className='waving-hand'>👋</span> </p>
-            <p className='hero_tag text-gray_gradient'> Building Products & Transforming Lives</p>
+            <p className='hero_tag text-gray_gradient text-wrap'> Building Products & Transforming Lives</p>
         </div>
 
         <div className='w-full h-full absolute inset-0'>
